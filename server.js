84eff6179e47068755da022ceaa063ddef1df26a
Yuehao Wang <352209095@qq.com>
2017-07-25 23:20:21 +0800

    初始化

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
 const port = process.env.PORT || 1234;
 
-app.use(express.static(path.join(__dirname, '/static')));
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, './static/index.html'));
-});
+app.use(express.static('static'));
 
 const server = app.listen(port, () => {
   console.log(`technode is on port ${port} |`);
@@ -16,12 +12,15 @@ const server = app.listen(port, () => {
 const io = require('socket.io').listen(server);
 
 io.sockets.on('connection', (socket) => {
+  console.log(1)
   socket.on('set nickname', (name) => {
+    console.log(2)
     socket.nickname = name;
     socket.broadcast.emit('new user', {
       nickname: name,
       type: 'user',
     });
+    console.log(3)
     socket.emit('login', {
       nickname: name,
       id: socket.id,
