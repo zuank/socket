cfa0c9ccbf58d186b133509affcb179318aee8ab
wangyuehao <wangyuehao@jiangduoduo.com>
2018-10-26 16:41:51 +0800

    修复超时登出

--- server.js
+++ server.js
@@ -6,28 +6,32 @@ const port = process.env.PORT || 1234;
 app.use(express.static('static'));
 
 const server = app.listen(port, () => {
-  console.log(`technode is on port ${port} |`);
+    console.log(`technode is on port ${port}`);
 });
 
 const io = require('socket.io').listen(server);
 
 io.sockets.on('connection', (socket) => {
-  socket.on('set nickname', (name) => {
-    socket.nickname = name;
-    socket.broadcast.emit('new user', {
-      nickname: name,
-      type: 'user',
+    socket.on('set nickname', (name) => {
+        socket.nickname = name;
+        socket.broadcast.emit('new user', {
+            nickname: name,
+            type: 'user',
+        });
+        socket.emit('login', {
+            nickname: name,
+            id: socket.id,
+        });
     });
-    socket.emit('login', {
-      nickname: name,
-      id: socket.id,
+    socket.on('new dialog', (str) => {
+        if (!socket.nickname) {
+            socket.emit('need login');
+            return
+        }
+        io.emit('new dialog', {
+            value: str,
+            nickname: socket.nickname,
+            type: 'dialog',
+        });
     });
-  });
-  socket.on('new dialog', (str) => {
-    io.emit('new dialog', {
-      value: str,
-      nickname: socket.nickname,
-      type: 'dialog',
-    });
-  });
 });
